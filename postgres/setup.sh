d7de45c5aab6967ab03e4f173812494e06830967
Vinicius Correa <viniciuscorrea91@gmail.com>
2015-11-05 17:16:19 -0200

    Added subscription database

--- postgres/setup.sh
+++ postgres/setup.sh
@@ -9,4 +9,8 @@ createdb -h ${DB_PORT_5432_TCP_ADDR} -p ${DB_PORT_5432_TCP_PORT} -U postgres war
 echo "[INFO] Executing create tables"
 psql -h ${DB_PORT_5432_TCP_ADDR} -p ${DB_PORT_5432_TCP_PORT} -U postgres -d warehouse -a -f /warehouse.sql
 
+createdb -h ${DB_PORT_5432_TCP_ADDR} -p ${DB_PORT_5432_TCP_PORT} -U postgres subscription
+# create tables
+echo "[INFO] Executing create tables"
+psql -h ${DB_PORT_5432_TCP_ADDR} -p ${DB_PORT_5432_TCP_PORT} -U postgres -d warehouse -a -f /subscription.sql
 #others - add creatdb line and after psql line for your service
