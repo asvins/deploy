fb5e9cf6786dd9d186b6269151a4913ae66452b5
Rafael Leite <r.cmgleite@gmail.com>
2015-11-14 03:31:02 -0200

    warehouse tables updated

--- postgres/warehouse.sql
+++ postgres/warehouse.sql
@@ -4,15 +4,46 @@ CREATE TABLE products (
 	type INTEGER,
 	description TEXT,
 	curr_quantity INTEGER,
-	min_quantity INTEGER 
-);
-
-CREATE TABLE order_products (
-	order_id INTEGER,
-	product_id INTEGER
+	min_quantity INTEGER,
+	curr_value REAL
 );
 
 CREATE TABLE orders (
 	id serial primary key,
-	approved BOOLEAN 
+	created_at INTEGER,
+	closed_at INTEGER,
+	approved BOOLEAN, 
+	canceled BOOLEAN
+);
+
+CREATE TABLE order_products (
+	order_id INTEGER REFERENCES orders(id),
+	product_id INTEGER REFERENCES products(id)
+);
+
+
+CREATE TABLE purchases (
+	id serial primary key,
+	created_at INTEGER,
+	confirmed_at INTEGER,
+	concluded_at INTEGER,
+	total_value REAL,
+	order_id INTEGER REFERENCES orders(id),
+
+);
+
+CREATE TABLE invoices (
+	id serial primary key,
+	product_id INTEGER REFERENCES products(id),
+	value REAL,
+	quantity INTEGER,
+	purchase_id INTEGER REFERENCES purchases(id)
+);
+
+CREATE TABLE withdrawal (
+	id serial primary key,
+	product_id INTEGER REFERENCES products(id),
+	quantity INTEGER,
+	issued_at INTEGER,
+	approved_at INTEGER
 );
