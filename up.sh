0b3a9f1c13d792fb5be757bdfe81cea400114b72
Rafael Leite <r.cmgleite@gmail.com>
2015-12-09 19:52:10 -0200

    fix on up.sh, and updating topics

--- up.sh
+++ up.sh
@@ -10,11 +10,19 @@ docker-compose up kafka > /dev/null 2>&1 &
 sleep 15
 echo "[INFO] Done!"
 
-echo "[INFO] ./create_topics.sh" &
-./create_topics.sh
-sleep 15
+echo "[INFO] ./create_topics.sh" 
+./create_topics.sh &
+sleep 30
 echo "[INFO] Done!"
 
+echo "[INFO] docker-compose up postgres"
+docker-compose up postgres &
+sleep 30
+echo "[INFO] Done!"
+
+echo "[INFO] docker-compose up postgres_setup"
+docker-compose up postgres_setup
+echo "[INFO] Done!"
 
 echo "[INFO] docker-compose up"
 docker-compose up 
